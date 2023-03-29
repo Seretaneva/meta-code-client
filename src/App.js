e5b3c3ffd7097a121e73cf9e5b1b2b45f81df14a
Petru.Lazari <petru.lazari@crossinx.com>
2023-03-29 18:59:09 +0200

    Init funcitonality for public view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import AdminNavigation from './components/admin/AdminNavigation'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import TopicsTable from './components/admin/pages/topics/TopicsTable'
 import ChaptersPage from './components/admin/pages/chapters/ChaptersPage'
 import TopicsPage from './components/admin/pages/topics/TopicsPage'
@@ -8,14 +8,20 @@ import AdminMainPage from './components/admin/pages/adminMain/adminMainPage'
 import LessonsPage from './components/admin/pages/lessons/LessonsPage'
 import AboutPage from './components/admin/pages/about/AboutPage'
 import ProblemPage from './components/admin/pages/problems/ProblemPage'
-import { useState } from 'react'
 import LoginPage from './components/admin/pages/authenticate/LoginPage'
+import SponsorsPage from './components/admin/pages/sponsors/SponsorsPage'
+import PublicNavigation from './components/public/PublicNavigation'
+import TopicsNavigation from './components/public/TopicsNavigation'
+import PublicFooter from './components/public/PublicFooter'
 
 function App () {
+  const location = useLocation();
+  const isAdminPage = location.pathname.startsWith('/admin');
   return (
     <div className='App'>
-      <Router>
-        <AdminNavigation />
+  
+      {isAdminPage ? <AdminNavigation /> : <PublicNavigation/>}
+      {isAdminPage ? null : <TopicsNavigation/> }
         <div className='container'>
           <Routes>
             <Route
@@ -38,14 +44,27 @@ function App () {
               path='/admin/problems'
               element={<ProblemPage name='problems' />}
             />
+             <Route
+              path='/admin/sponsors'
+              element={<SponsorsPage name='sponsors' />}
+            />
             <Route path='/admin/about' element={<AboutPage />} />
             <Route path='/admin' element={<AdminMainPage />} />
             <Route path='/admin/login' element={<LoginPage />} />
           </Routes>
+          <PublicFooter></PublicFooter>
         </div>
-      </Router>
+
     </div>
   )
 }
 
-export default App
+function Main() {
+  return (
+    <Router>
+      <App />
+    </Router>
+  );
+}
+
+export default Main
