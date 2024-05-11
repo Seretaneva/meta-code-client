ebb63ace4549f1b1b9dc37c3b5f064293d54c41f
Seretaneva <seretan30@gmail.com>
2024-05-11 23:51:10 +0300

    FIX STYLES

--- src/components/public/PublicFooter.js
+++ src/components/public/PublicFooter.js
@@ -32,26 +32,26 @@ function PublicFooter () {
   return (
     <div className={styles.footerContainer}>
       <footer className='footer'>
-        <div className='footer_container'>
+        <div className={styles.footer_container}>
           <div className='row'>
             <div className='col-md-4'>
-              <h3>Despre FdC</h3>
-              <p>{infoHome.aboutFooter}</p>
+              <h3 className = {styles.h3_footer}>Despre FdC</h3>
+              <p className={styles.footerparagraph}>{infoHome.aboutFooter}</p>
             </div>
             <div className='col-md-4'>
-              <h3>Donează</h3>
-              <p>{infoHome.donateFooterContent}</p>
-              <button className='btn btn-success' onClick={openDonationPopup}>
+              <h3 className={styles.h3_footer}>Donează</h3>
+              <p className={styles.footerparagraph}>{infoHome.donateFooterContent}</p>
+              <button className='btn btn-success' style={{ marginLeft: '80px' , paddingLeft: '10px' }} onClick={openDonationPopup}>
                 Donează acum!
               </button>
             </div>
             <div className='col-md-4'>
-              <h3>Contact</h3>
-              <p>{infoHome.contactFooterContent}</p>
+              <h3 className={styles.h3_footer}>Contact</h3>
+              <p className={styles.footerparagraph}>{infoHome.contactFooterContent}</p>
             </div>
           </div>
         </div>
-        <hr />
+        <hr className= {styles.hr_style} />
         <div className='row justify-content-center'>
           <div className='col-md-12 text-center'>
             <p>{infoHome.ownerInfo}</p>
